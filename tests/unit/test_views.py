0bcb0094bf87ebcbc5dc1c0563428c0c7760d1b0
Jasper Berghoef <jasper.berghoef@gmail.com>
2018-08-11 11:48:46 +0200

    Test manage view

---
+++ tests/unit/test_views.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_manage_view(client):
+    url = reverse('wtm:manage')
+
+    response = client.get(url)
+    assert response.status_code == 200
+
+    response = client.post(url)
+    assert response.status_code == 302
