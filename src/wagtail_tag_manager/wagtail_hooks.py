741e9536881e9166a51ee211fe86720cc1220412
Jasper Berghoef <jasper.berghoef@gmail.com>
2018-09-07 22:23:06 +0200

    Add descriptions to model admin

--- src/wagtail_tag_manager/wagtail_hooks.py
+++ src/wagtail_tag_manager/wagtail_hooks.py
@@ -1,52 +1,83 @@
+from django.utils.html import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django.templatetags.static import static
+from django.template.defaultfilters import truncatechars
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register)
 
-from wagtail_tag_manager.models import Tag, Constant, Variable, Trigger
+from wagtail_tag_manager.models import Tag, Trigger, Constant, Variable
 
 
 class ConstantModelAdmin(ModelAdmin):
     model = Constant
     menu_icon = 'snippet'
-    list_display = ('name', 'key', 'value',)
+    list_display = ('name_display', 'key', 'value',)
     search_fields = ('name', 'key', 'value', 'description',)
+
+    def name_display(self, obj):
+        if obj.description:
+            description = truncatechars(obj.description, 64)
+            return mark_safe(f"{obj.name}<br/><small>{description}</small>")
+        return obj.name
+    name_display.short_description = _('Name')
 
 
 class VariableModelAdmin(ModelAdmin):
     model = Variable
     menu_icon = 'snippet'
-    list_display = ('name', 'key', 'variable_type',)
+    list_display = ('name_display', 'key', 'variable_type',)
     list_filter = ('variable_type',)
     search_fields = ('name', 'key', 'description',)
     form_view_extra_js = [static('variable_form_view.bundle.js')]
+
+    def name_display(self, obj):
+        if obj.description:
+            description = truncatechars(obj.description, 64)
+            return mark_safe(f"{obj.name}<br/><small>{description}</small>")
+        return obj.name
+    name_display.short_description = _('Name')
 
 
 class TagModelAdmin(ModelAdmin):
     model = Tag
     menu_icon = 'code'
-    list_display = ('name', 'tag_type', 'tag_location', 'tag_loading', 'active',)
+    list_display = (
+        'name_display', 'tag_type', 'tag_location', 'tag_loading', 'active',)
     list_filter = ('active', 'tag_type', 'tag_location', 'tag_loading',)
     search_fields = ('name', 'description', 'content',)
     form_view_extra_js = [static('tag_form_view.bundle.js')]
+
+    def name_display(self, obj):
+        if obj.description:
+            description = truncatechars(obj.description, 64)
+            return mark_safe(f"{obj.name}<br/><small>{description}</small>")
+        return obj.name
+    name_display.short_description = _('Name')
 
 
 class TriggerModelAdmin(ModelAdmin):
     model = Trigger
     menu_icon = 'media'
-    list_display = ('name', 'tags_count', 'active',)
+    list_display = ('name_display', 'tags_count', 'active',)
     list_filter = ('active',)
     search_fields = ('name', 'description',)
 
+    def name_display(self, obj):
+        if obj.description:
+            description = truncatechars(obj.description, 64)
+            return mark_safe(f"{obj.name}<br/><small>{description}</small>")
+        return obj.name
+    name_display.short_description = _('Name')
+
     def tags_count(self, obj):
         return f"{obj.tags.count()} tag(s)"
-    tags_count.short_description = 'Tags'
+    tags_count.short_description = _('Tags')
 
 
 class TagManagerAdminGroup(ModelAdminGroup):
     menu_label = _("Tag Manager")
     menu_icon = 'code'
-    menu_order = 1000
+    menu_order = 640
     items = (
         TagModelAdmin, ConstantModelAdmin, VariableModelAdmin,
         TriggerModelAdmin)
---
+++ tests/unit/test_wagtail_hooks.py
@@ -0,0 +1,76 @@
+import pytest
+
+from tests.factories.constant import ConstantFactory
+from tests.factories.variable import VariableFactory
+from tests.factories.tag import TagFactory
+from tests.factories.trigger import TriggerFactory
+
+from wagtail_tag_manager.wagtail_hooks import (
+    ConstantModelAdmin, VariableModelAdmin, TagModelAdmin, TriggerModelAdmin)
+
+
+@pytest.mark.django_db
+def test_name_display():
+    constant = ConstantFactory(description='Test')
+    constant_model_admin = ConstantModelAdmin()
+
+    description = constant_model_admin.name_display(constant)
+    assert constant.description in description
+
+    variable = VariableFactory(key='var', description='Test')
+    variable_model_admin = VariableModelAdmin()
+
+    description = variable_model_admin.name_display(variable)
+    assert variable.description in description
+
+    tag = TagFactory(description='Test')
+    tag_model_admin = TagModelAdmin()
+
+    description = tag_model_admin.name_display(tag)
+    assert tag.description in description
+
+    trigger = TriggerFactory(description='Test')
+    trigger_model_admin = TriggerModelAdmin()
+
+    description = trigger_model_admin.name_display(trigger)
+    assert tag.description in description
+
+
+@pytest.mark.django_db
+def test_no_description_display():
+    constant = ConstantFactory()
+    constant_model_admin = ConstantModelAdmin()
+
+    description = constant_model_admin.name_display(constant)
+    assert constant.name == description
+
+    variable = VariableFactory(key='var')
+    variable_model_admin = VariableModelAdmin()
+
+    description = variable_model_admin.name_display(variable)
+    assert variable.name == description
+
+    tag = TagFactory()
+    tag_model_admin = TagModelAdmin()
+
+    description = tag_model_admin.name_display(tag)
+    assert tag.name == description
+
+    trigger = TriggerFactory()
+    trigger_model_admin = TriggerModelAdmin()
+
+    description = trigger_model_admin.name_display(trigger)
+    assert trigger.name == description
+
+
+@pytest.mark.django_db
+def test_tag_count():
+    tag1 = TagFactory(name='tag1')
+    tag2 = TagFactory(name='tag2')
+
+    trigger = TriggerFactory()
+    trigger.tags.add(tag1, tag2)
+    trigger_model_admin = TriggerModelAdmin()
+
+    result = trigger_model_admin.tags_count(trigger)
+    assert result == '2 tag(s)'
