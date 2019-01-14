650fcf751951393a985e29f58d887b3f111adf65
Tobias Englert <te@englert.it>
2019-01-14 23:58:19 +0100

    fix: Typo in context processor

--- src/wagtail_tag_manager/context_processors.py
+++ src/wagtail_tag_manager/context_processors.py
@@ -3,6 +3,6 @@ from wagtail_tag_manager.settings import StaticFilesSettings
 
 def export_static_files_settings(request):
     return {
-        "WMT_INCLUDE_CSS": StaticFilesSettings.include_css,
-        "WMT_INCLUDE_JS": StaticFilesSettings.include_js,
+        "WTM_INCLUDE_CSS": StaticFilesSettings.include_css,
+        "WTM_INCLUDE_JS": StaticFilesSettings.include_js,
     }
