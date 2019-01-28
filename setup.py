587e14ef2a9fd6834af39331ebb7644f07d7c9a9
Jasper Berghoef <jasper.berghoef@gmail.com>
2019-01-28 14:49:13 +0100

    Revert wrongly bumped dependency

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = [
     "pytest-cov==2.4.0",
     "pytest-django==3.1.2",
     "pytest-pythonpath==0.7.2",
-    "pytest-sugar==0.8.1",
+    "pytest-sugar==0.8.0",
     "pytest==3.1.0",
     "wagtail_factories==1.0.0",
 ]
