c16923aee3568338e23509291957b52f8a33d48e
Jasper Berghoef <jasper.berghoef@gmail.com>
2019-01-30 19:07:51 +0100

    Add test for context processors

---
+++ tests/unit/test_context_processors.py
@@ -0,0 +1,11 @@
+import pytest
+
+from wagtail_tag_manager.context_processors import cookie_state
+
+
+@pytest.mark.django_db
+def test_cookie_state(rf, site):
+    request = rf.get(site.root_page.url)
+    assert cookie_state(request) == {
+        "wtm_cookie_state": {"analytical": True, "functional": True, "traceable": False}
+    }
