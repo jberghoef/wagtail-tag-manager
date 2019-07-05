ac0b2730d9157e2eddcc08e211b2307a03084883
Jasper Berghoef <jasper.berghoef@gmail.com>
2019-07-05 17:07:35 +0200

    Bump version: 0.19.1 → 0.20.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open('README.md') as fh:
 
 setup(
     name="wagtail-tag-manager",
-    version="0.19.1",
+    version="0.20.0",
     description="A Wagtail add-on for managing tags.",
     author="Jasper Berghoef",
     author_email="jasper.berghoef@gmail.com",
